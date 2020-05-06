f32a2338dc98302c099011834dc65222756eec74
Gentrit Abazi <gentritabazi01@gmail.com>
2020-05-06 23:50:34 +0000

    Apply fixes from StyleCI

--- auto-contribution.php
+++ auto-contribution.php
@@ -1,4 +1,5 @@
 <?php
+
 // General variables
 $beginDate = '2017-01-05';
 $endDate = '2017-01-10';
@@ -11,21 +12,21 @@ $fileName = 'test-file.md';
 $setOrExport = 'set';
 
 // Loop between two dates
-foreach($period as $dt) {
+foreach ($period as $dt) {
     $d = $dt->format('Y-m-d');
 
     // Random contributions
-    for($i = 0; $i < rand(1, 35); $i++) {
+    for ($i = 0; $i < rand(1, 35); $i++) {
         // Write to file
-        $myfile = fopen($fileName, 'w') or die("Unable to open file!");
-        fwrite($myfile, $d. ' - '. $i);
+        $myfile = fopen($fileName, 'w') or die('Unable to open file!');
+        fwrite($myfile, $d.' - '.$i);
         fclose($myfile);
 
         // Commit changes
-        exec($setOrExport. ' GIT_COMMITTER_DATE="'. $d. ' 12:00:00"');
-        exec($setOrExport. ' GIT_AUTHOR_DATE="'. $d. ' 12:00:00"');
-        exec('git add '. $fileName. ' -f');
-        exec('git commit --date="'. $d. ' 12:00:00" -m "commited on '. $d. '"');
+        exec($setOrExport.' GIT_COMMITTER_DATE="'.$d.' 12:00:00"');
+        exec($setOrExport.' GIT_AUTHOR_DATE="'.$d.' 12:00:00"');
+        exec('git add '.$fileName.' -f');
+        exec('git commit --date="'.$d.' 12:00:00" -m "commited on '.$d.'"');
     }
 }
 
