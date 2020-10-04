9afaf146bd2a5bbe25680af1c02ac9032abe4810
Gentrit Abazi <35135482+gentritabazi01@users.noreply.github.com>
2020-10-04 12:33:31 +0200

    Update auto-contribution.php

--- auto-contribution.php
+++ auto-contribution.php
@@ -3,7 +3,6 @@
 // General variables
 $beginDate = '2017-01-05';
 $endDate = '2017-01-10';
-
 $begin = new DateTime($beginDate);
 $end = new DateTime($endDate);
 $interval = DateInterval::createFromDateString('1 day');
@@ -11,18 +10,18 @@ $period = new DatePeriod($begin, $interval, $end);
 $fileName = 'test-file.md';
 $setOrExport = 'set';
 
-// Loop between two dates
+// Loop between two dates.
 foreach ($period as $dt) {
     $d = $dt->format('Y-m-d');
 
-    // Random contributions
+    // Random contributions.
     for ($i = 0; $i < rand(1, 35); $i++) {
-        // Write to file
+        // Write to file,
         $myfile = fopen($fileName, 'w') or die('Unable to open file!');
         fwrite($myfile, $d.' - '.$i);
         fclose($myfile);
 
-        // Commit changes
+        // Commit changes,
         exec($setOrExport.' GIT_COMMITTER_DATE="'.$d.' 12:00:00"');
         exec($setOrExport.' GIT_AUTHOR_DATE="'.$d.' 12:00:00"');
         exec('git add '.$fileName.' -f');
@@ -30,7 +29,7 @@ foreach ($period as $dt) {
     }
 }
 
-// Final commands
+// Final commands.
 exec('git push origin master');
 exec('git commit -am "cleanup"');
 exec('git push origin master');
